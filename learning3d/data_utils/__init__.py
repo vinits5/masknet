107866d2846e9840f1f16d9dfa531d3bbdc0d83f
Vinit Sarode <vinitsarode5@gmail.com>
2020-10-29 18:42:20 +0530

    bug in data_utils import resolved

--- learning3d/data_utils/__init__.py
+++ learning3d/data_utils/__init__.py
@@ -1,3 +1,3 @@
-from .dataloaders import ModelNet40Data, KittiDataset, AnyData, UserData
+from .dataloaders import ModelNet40Data, AnyData, UserData
 from .dataloaders import ClassificationData, RegistrationData, SegmentationData, FlowData, SceneflowDataset
 from .dataloaders import download_modelnet40
